a21ccbbb55b62a7939cb323082637627a7bfda5a
Lee Treveil <leetreveil@gmail.com>
2014-01-17 15:34:57 +0000

    WIP

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"fmt"
 	_ "log"
 )
 
 func main() {
-	// fmt.Println("Hello, world")
-	// ssdp.TestMe()
 	TestMe()
+	fmt.Println("searching for devices to play to...")
+	fmt.Println("now controlling [TV]UE04E20RRZZE")
+
+	// commands := []string{"control", "play", "pause", "next", "prev", "search"}
+
+	var d string
+	for {
+		_, err := fmt.Scanln(&d)
+		if err != nil {
+			break
+		}
+		fmt.Println(d)
+	}
+
+	// fmt.Println("Hello, world")
+
+	// Parse("haggis")
+	// CreateSoapRequest("a", "b", "c")
 	// log.Fatal(fileserver.Serve(
 	//  ":8080",
 	//  "/Users/leetreveil/Downloads/Game.of.Thrones.S03E06.HDTV.x264-2HD.mp4",
 	// ))
+
+	// GetProtocolInfo()
 }
